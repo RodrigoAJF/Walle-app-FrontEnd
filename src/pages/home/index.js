b5c6adbda5c9bc3a247293bc725da26f26f2ca5e
RodrigoAJF <rodrigoalvesjacomefilho@gmail.com>
2023-11-06 20:57:40 -0300

    feat: implement header on page.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,29 @@
-window.onload = () => {
+const onloadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
-  console.log(email);
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navbarUseInfo = document.getElementById("navbar-user-container");
+  const navbarUserAvatar = document.getElementById("navbar-user-avatar");
+
+  //add user email.
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+  navbarUseInfo.appendChild(emailElement);
+
+  //add logout link.
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("Sair");
+  logoutElement.appendChild(logoutText);
+  navbarUseInfo.appendChild(logoutElement);
+
+  //add user first letter inside avatar.
+  const nameElement = document.createElement("h3");
+  const nameText = document.createTextNode(name.charAt(0));
+  nameElement.appendChild(nameText);
+  navbarUserAvatar.appendChild(nameElement);
+};
+
+window.onload = () => {
+  onloadUserInfo();
 };
