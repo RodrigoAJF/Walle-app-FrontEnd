c9bb3cbe55b7453931ab244211df60760799de24
RodrigoAJF <rodrigoalvesjacomefilho@gmail.com>
2023-11-08 20:19:14 -0300

    feat: implement finance cards

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,85 @@
+const renderFinancesElemnts = (data) => {
+  const totalItems = data.length;
+  const revenues = data
+    .filter((item) => Number(item.value) > 0)
+    .reduce((acc, item) => acc + Number(item.value), 0);
+  const expenses = data
+    .filter((item) => Number(item.value) < 0)
+    .reduce((acc, item) => acc + Number(item.value), 0);
+  const totalvalue = revenues + expenses;
+
+  // render total items
+  const financeCard1 = document.getElementById("finance-card-1");
+  const totalText = document.createTextNode(totalItems);
+  const totalElement = document.createElement("h1");
+  totalElement.className = "mt small";
+  totalElement.appendChild(totalText);
+  financeCard1.appendChild(totalElement);
+
+  // render revenue
+  const financeCard2 = document.getElementById("finance-card-2");
+  const revenueText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(revenues)
+  );
+  const revenueTextElement = document.createElement("h1");
+  revenueTextElement.className = "mt small";
+  revenueTextElement.style.color = "#5936cd";
+  revenueTextElement.appendChild(revenueText);
+  financeCard2.appendChild(revenueTextElement);
+
+  // render expenses
+  const financeCard3 = document.getElementById("finance-card-3");
+  const expensesText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(expenses)
+  );
+  const expensesTextElement = document.createElement("h1");
+  expensesTextElement.className = "mt small";
+  expensesTextElement.style.color = "red";
+  expensesTextElement.appendChild(expensesText);
+  financeCard3.appendChild(expensesTextElement);
+
+  // render balance
+  const financeCard4 = document.getElementById("finance-card-4");
+  const balanceText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(totalvalue)
+  );
+  const balanceTextElement = document.createElement("h1");
+  balanceTextElement.className = "mt small";
+  balanceTextElement.style.color = "green";
+  balanceTextElement.appendChild(balanceText);
+  financeCard4.appendChild(balanceTextElement);
+};
+
+const onloadFinancesData = async () => {
+  try {
+    const date = "2023-11-06";
+    const email = localStorage.getItem("@WalletApp:userEmail");
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/finances?date=${date}`,
+      {
+        method: "GET",
+        headers: {
+          email: email,
+        },
+      }
+    );
+    const data = await result.json();
+    renderFinancesElemnts(data);
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
+
 const onloadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
   const name = localStorage.getItem("@WalletApp:userName");
@@ -26,4 +108,5 @@ const onloadUserInfo = () => {
 
 window.onload = () => {
   onloadUserInfo();
+  onloadFinancesData();
 };
